047732eff9eee4a3a7ad869d46fae7d4ec86946d
lincoln <lincoln-md@hotmail.com>
2022-07-24 12:31:29 -0300

    Improving Developer Experience

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import graphql from '@rollup/plugin-graphql'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), graphql()]
 })
+
+// For Vue CLI apps
+
+// vue.config.js
+
+// module.exports = {
+//   chainWebpack: config => {
+//     config.module
+//       .rule('graphql')
+//       .test(/\.(graphql|gql)$/)
+//       .use('graphql-tag/loader')
+//       .loader('graphql-tag/loader')
+//       .end()
+//   },
+// }
